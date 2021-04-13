d94096b55603a1394ddd184ae52626aa3ad7b77e
seung-ik <69374857+seung-ik@users.noreply.github.com>
2021-04-13 23:58:14 +0900

    front setting

---
+++ front/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Route,Switch,Redirect} from 'react-router-dom'
+import loadable from '@loadable/component'
+
+// const Calendar = loadable(()=> import("./components/Calendar"))
+// const Main = loadable(()=> import("./components/Main"))
+
+function App() {
+  return (
+    <Switch>
+      {/* <Route exact path="/" component={Main}/>
+      <Route path="/calendar" component={Calendar}/> */}
+    </Switch>
+  );
+}
+
+export default App;
